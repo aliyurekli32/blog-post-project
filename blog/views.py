dca2f274cb838a5ed297a23d927ef5be89f13863
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-03 11:42:08 +0300

    Fix: django-filters issue

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Category, Blog
 from .serializers import CategorySerializer, BlogSerializer
+
 # Create your views here.
 
 class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    filter_fields = ['name']
+    filterset_fields = ['name']
     
 class BlogView(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    filterset_fields = ['category']
+    search_fields = ['title', 'content']
